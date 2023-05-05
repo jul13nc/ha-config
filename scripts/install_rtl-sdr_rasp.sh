8b0d6c4c41c102ad7c63d539513d7fdd79fba23a
jul13nc <julien@MacBook-Pro-de-Julien.local>
2023-05-05 14:18:18 +0200

    Begin script - only stl-sdr install

---
+++ scripts/install_rtl-sdr_rasp.sh
@@ -0,0 +1,14 @@
+sudo -i
+apt install -y ncurses-dev cmake librtlsdr-dev 
+apt install -y git
+apt install -y libtool-bin automake make info libc6-dev libgmp-dev libgmp3-dev gawk qpdf bison 
+git clone git://git.osmocom.org/rtl-sdr.git 
+cd rte-sdr
+mkdir build
+cd build
+cmake ../ -DINSTALL_UDEV_RULES=ON 
+make 
+make install 
+ldconfig
+cp ../rtl-sdr.rules /etc/udev/rules.d
+ln -s /usr/local/bin/rtl-sdr /usr/bin/rtl-sdr
