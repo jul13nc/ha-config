b2bf4859d1a16c1bb0d51671c3f951a357a062b2
jul13nc <jul13nc@users.noreply.github.com>
2023-05-05 14:36:09 +0200

    Typo in dir name

--- scripts/install_rtl-sdr_rasp.sh
+++ scripts/install_rtl-sdr_rasp.sh
@@ -3,7 +3,7 @@ apt install -y ncurses-dev cmake librtlsdr-dev
 apt install -y git
 apt install -y libtool-bin automake make info libc6-dev libgmp-dev libgmp3-dev gawk qpdf bison 
 git clone git://git.osmocom.org/rtl-sdr.git 
-cd rte-sdr
+cd rtl-sdr
 mkdir build
 cd build
 cmake ../ -DINSTALL_UDEV_RULES=ON 
